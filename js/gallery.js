5f12ccd218908b385bcb364333cd831083491dc8
Alisa <alisa.sp@gmail.com>
2018-07-03 01:54:23 +0300

    24 Личный проект: перламутровые пуговицы

--- js/gallery.js
+++ js/gallery.js
@@ -1,14 +1,66 @@
 'use strict';
 
 (function () {
-  var renderGallery = function (pictures) {
-    var picturesList = document.querySelector('.pictures');
+  var NEW_IMAGES_NUMBER = 10;
+  var picturesList = document.querySelector('.pictures');
+  var imagesFilters = document.querySelector('.img-filters');
+  var filtersForm = imagesFilters.querySelector('.img-filters__form');
+  var activeClass = 'img-filters__button--active';
+  var filtersMap;
+
+  var onLoad = function (pictures) {
+    renderThumbnails(pictures);
+    imagesFilters.classList.remove('img-filters--inactive');
+    filtersForm.addEventListener('click', onfiltersFormClick);
+    filtersMap = {
+      'filter-popular': pictures,
+      'filter-new': window.utils.getRandomArray(pictures, NEW_IMAGES_NUMBER),
+      'filter-discussed': sortByComments(pictures)
+    };
+  };
+
+  var renderThumbnails = function (pictures) {
     var fragment = document.createDocumentFragment();
 
-    for (var i = 0; i < pictures.length; i++) {
-      fragment.appendChild(window.renderThumbnail(pictures[i]));
-    }
+    pictures.forEach(function (picture) {
+      fragment.appendChild(window.renderThumbnail(picture));
+    });
     picturesList.appendChild(fragment);
+  };
+
+  var sortByComments = function (arr) {
+    var arr2 = arr.slice();
+    arr2.sort(function (left, right) {
+      return right.comments.length - left.comments.length;
+    });
+    return arr2;
+  };
+
+  var removeThumbnails = function () {
+    var tmpRange = document.createRange();
+    var start = picturesList.querySelector('.img-upload');
+
+    tmpRange.selectNodeContents(picturesList);
+    tmpRange.setStart(start, 2);
+    tmpRange.deleteContents();
+
+    tmpRange.detach();
+  };
+
+  var onfiltersFormClick = function (evt) {
+    if (evt.target.tagName === 'BUTTON') {
+      filtersForm.querySelector('.' + activeClass)
+        .classList.remove(activeClass);
+      evt.target.classList.add(activeClass);
+      updateGallery(filtersMap[evt.target.id]);
+    }
+  };
+
+  var updateGallery = function (filter) {
+    window.utils.debounce(function () {
+      removeThumbnails();
+      renderThumbnails(filter);
+    });
   };
 
   var onError = function (message) {
@@ -21,5 +73,6 @@
     document.body.insertAdjacentElement('afterbegin', node);
   };
 
-  window.backend.download(renderGallery, onError);
+  window.backend.download(onLoad, onError);
+
 }());
