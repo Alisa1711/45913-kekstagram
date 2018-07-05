e69ce206d14fb64a7f0e7242a36a7491d1d2f83a
Alisa1711 <36865368+Alisa1711@users.noreply.github.com>
2018-07-05 13:44:28 +0300

    Update validate-hashtags.js

--- js/validate-hashtags.js
+++ js/validate-hashtags.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-  var restriction = {
+  var RESTRICTION = {
     NUMBER: 5,
     LENGTH: 20
   };
@@ -13,8 +13,8 @@
     if (window.utils.checkMatchingItems(hashtags)) {
       validityMessages.push('Нельзя использовать два одинаковых хеш-тега (хеш-теги нечувствительны к регистру)');
     }
-    if (hashtags.length > restriction.NUMBER) {
-      validityMessages.push('Нельзя указать больше' + restriction.NUMBER + 'хэш-тегов');
+    if (hashtags.length > RESTRICTION.NUMBER) {
+      validityMessages.push('Нельзя указать больше' + RESTRICTION.NUMBER + 'хэш-тегов');
     }
     for (var i = 0; i < hashtags.length; i++) {
       var hashtag = hashtags[i];
@@ -28,8 +28,8 @@
       if (hashtag === '#') {
         validityMessages.push('Хеш-тег не может состоять только из одной решётки');
       }
-      if (hashtag.length > restriction.LENGTH) {
-        validityMessages.push('Максимальная длина хэш-тега' + restriction.LENGTH + 'символов, включая решётку');
+      if (hashtag.length > RESTRICTION.LENGTH) {
+        validityMessages.push('Максимальная длина хэш-тега' + RESTRICTION.LENGTH + 'символов, включая решётку');
       }
     }
     return window.utils.getUniqueItems(validityMessages).join('. ');
